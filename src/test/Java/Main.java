64d7d834c4e62214abbee91c31a885c730f5b2be
xSkat <19041996zx@mail.ru>
2026-04-25 12:40:31 +0300

    commit main)

--- src/test/Java/Main.java
+++ src/test/Java/Main.java
@@ -1,6 +1,7 @@
 public class Main {
 
     public static void main(String... args) {
+
         // 0) Арифметические операции над двумя int
         System.out.println("=== 0) Операции над int ===");
         int a = 123;
